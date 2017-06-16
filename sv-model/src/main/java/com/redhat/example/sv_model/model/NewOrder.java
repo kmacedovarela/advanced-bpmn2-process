e5cf50a984a1405f66e5f1cf6208e30acc0c9e8e
Administrator <>
2017-06-16 16:43:17 -0300

    {/sv-model/src/main/java/com/redhat/example/sv_model/model/NewOrder.java}

--- sv-model/src/main/java/com/redhat/example/sv_model/model/NewOrder.java
+++ sv-model/src/main/java/com/redhat/example/sv_model/model/NewOrder.java
@@ -43,7 +43,7 @@ public class NewOrder implements java.io.Serializable
 
    public String toString()
    {
-      return "New Order Date: " + this.isHoaApprovalHoaMeetingDate() + ", Hoa approval:" + this.isHoaApproval();
+      return "New Order Date: " + this.getHoaMeetingDate() + ", Hoa approval:" + this.isHoaApproval();
 
    }
 
