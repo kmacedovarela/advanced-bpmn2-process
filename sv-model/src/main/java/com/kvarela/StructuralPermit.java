ffae75bb6316e3cda6adf30d81f4938d5f646f6d
bpms <>
2017-07-14 19:21:41 -0300

    {/sv-model/src/main/java/com/kvarela/StructuralPermit.java}

--- sv-model/src/main/java/com/kvarela/StructuralPermit.java
+++ sv-model/src/main/java/com/kvarela/StructuralPermit.java
@@ -5,7 +5,7 @@ package com.kvarela;
  */
 
 @javax.persistence.Entity
-public class StructuralPermit extends com.kvarela.Permit implements java.io.Serializable 
+public class StructuralPermit implements java.io.Serializable 
 {
 
    static final long serialVersionUID = 1L;
